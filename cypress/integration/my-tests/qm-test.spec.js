5a6a33f70dfeb3c76f64aa3eab6b884994bff5e6
jendi94 <jedrek_antkiewicz@yahoo.pl>
2022-03-20 01:08:55 +0100

    Deleted comments

--- cypress/integration/my-tests/qm-test.spec.js
+++ cypress/integration/my-tests/qm-test.spec.js
@@ -1,15 +1,7 @@
-// test-qm.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
-/// <reference types="Cypress" />
+
 const path = require('path')
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
   return false
 })
 
